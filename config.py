01c6c571f0ee90f94332a23f3c4b82e8cfeb91a1
HABIBI-001-10 <habeebcva8606@gmail.com>
2026-01-19 22:39:33 +0530

    config file

---
+++ config.py
@@ -0,0 +1,9 @@
+api_id = 0 # your_api_id_here remove the zero.
+api_hash = "your_api_hash_here"
+
+SPAM_THRESHOLD = 0.56
+MESSAGE_CHECK_LIMIT = 5
+MAX_LEAVES_PER_RUN = 15   # Recommended safe limit
+DELAY_SECONDS = 30        # Recommended safe delay
+
+SAFE_CHANNELS = ["Family", "College", "Work", "Movies"]
